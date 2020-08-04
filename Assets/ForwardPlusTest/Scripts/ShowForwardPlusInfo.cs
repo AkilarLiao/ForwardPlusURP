bc69dff65e29e9b0c177f959c67b26e5bb83af68
akilarliao <lee.akilar@gmail.com>
2020-08-04 15:24:04 +0800

    1.reDesign folder architecture. 2.append display light count information.

---
+++ Assets/ForwardPlusTest/Scripts/ShowForwardPlusInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using CustomRender;
+
+public class ShowForwardPlusInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        DisplayFPS displayFPS = GetComponent<DisplayFPS>();
+        if (displayFPS != null)
+            displayFPS.AppendExtendString += AppendExtendStringCB;
+    }
+    private void OnDestroy()
+    {
+        DisplayFPS displayFPS = GetComponent<DisplayFPS>();
+        if (displayFPS != null)
+            displayFPS.AppendExtendString -= AppendExtendStringCB;
+    }
+
+    private void AppendExtendStringCB(out string text)
+    {
+        text = string.Format("\nCurrentLightCount{0}",
+            TileLightCullingPass.GetCurrentLightCount());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {   
+    }
+}
